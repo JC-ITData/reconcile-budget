fce6e12e8c45bdc20877a0c0c12a4f61eda6d1bc
JC_ITData <judy.cazares@protonmail.com>
2025-08-14 18:56:10 -0500

    fix loop through files, union join

--- budget_reconciler/utils/FileReader.py
+++ budget_reconciler/utils/FileReader.py
@@ -12,7 +12,7 @@ class FileReader(ABC):
     def read(self, file_path: str, file_desc: str = '', **kwargs):
 
         file_desc = file_desc if file_desc else file_path.split('/')[-1]
-        return_data = None
+        results_list = []
 
         path_obj = Path(file_path)
         if not(path_obj.exists()):
@@ -21,16 +21,13 @@ class FileReader(ABC):
             raise FileNotFoundError(message)
         
         if (path_obj.is_dir()):
-            list_files = [ '\\'.join(path_obj.parts) + '\\' + x for x in os.listdir(file_path)]
+            list_files = [str(item) for item in path_obj.glob('*')]
         else :
-            list_files = [ '\\'.join(path_obj.parts) ]
-
-        print("list_files : " + str(list_files))
-        
+            list_files = [ str(path_obj), ]        
 
         for file_name in list_files:
             try:
-                return_data = self._read_file(file_name, **kwargs)
+                results_list.append(self._read_file(file_name, **kwargs))
             except FileNotFoundError:
                 logging.error(f"Error: {file_desc} was not found.")
                 raise
@@ -43,10 +40,15 @@ class FileReader(ABC):
             finally:
                 logging.info(f"{file_desc} read.")
 
+        return_data = self._combine_read_files(results_list)
         return return_data
 
     @abstractmethod
     def _read_file(self, file_path: str, **kwargs):
+        pass
+
+    @abstractmethod
+    def _combine_read_files(self, results_list):
         pass
 
 class JsonFileReader(FileReader):
@@ -58,6 +60,12 @@ class JsonFileReader(FileReader):
             logging.error(f"Error reading json file into hash : {file_path}\n{e}")
             raise
         return return_hash
+    
+    def _combine_read_files(self,results_list: list[dict]) -> dict :
+        return_dict = {}
+        for dict_item in results_list:
+            return_dict = {**return_dict, **dict_item}
+        return return_dict
 
 class CsvFileReader(FileReader):
     def _read_file(self, file_path: str, delimiter: str) -> pd.DataFrame:
@@ -67,4 +75,10 @@ class CsvFileReader(FileReader):
             logging.error(f"Error reading csv into pandas dataframe : {file_path}\n{e}")
             raise
         return return_df
-
+    
+    def _combine_read_files(self,results_list: list[pd.DataFrame]) -> pd.DataFrame:
+        return_df = pd.DataFrame()
+        for df in results_list :
+            # return_df = return_df.union(df, sort=False, all=True)
+            return_df = pd.concat([return_df,df],ignore_index=True)
+        return return_df
